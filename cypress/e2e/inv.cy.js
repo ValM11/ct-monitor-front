63e129332f8a7f2232af834d8690ee5eabd9520b
Valerie Martin <78977660+ValM11@users.noreply.github.com>
2022-12-16 11:06:54 +0100

    Update cypress test for investigator

--- cypress/e2e/inv.cy.js
+++ cypress/e2e/inv.cy.js
@@ -5,12 +5,12 @@ describe("Investigator functionalities", () => {
     cy.get(":nth-child(1) > .form-control").type("inv1@test.fr");
     cy.get(":nth-child(2) > .form-control").type("test1inv");
     cy.get("form > .btn").click();
-    cy.get(".list-group-item-action").click();
+    cy.get('[value="RDS1002"]').click();
     cy.get(":nth-child(1) > .btn").click();
     cy.get(":nth-child(2) > .col-sm-6 > .mb-3 > .form-control").type(5443223);
     cy.get(".btn").click();
     cy.get(":nth-child(1) > .nav-link").click();
-    cy.get(".list-group-item-action").click();
+    cy.get('[value="RDS1002"]').click();
     cy.get(":nth-child(2) > .btn").click();
     cy.get(".dropdown-toggle").click();
     cy.get(".dropdown-menu");
